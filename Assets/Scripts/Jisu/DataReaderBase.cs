7de92df528eb8bfbe62adfa489b1ef4b6f218ddb
SUHHAN <sky020403@gmail.com>
2024-07-04 22:00:16 +0900

    fit: 엑셀 연결 오류 해결

--- Assets/Scripts/Jisu/DataReaderBase.cs
+++ Assets/Scripts/Jisu/DataReaderBase.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public abstract class DataReaderBase : ScriptableObject
 {
-    [Header("시트의 주소")][SerializeField] public string associatedSheet = "1B1uFwuGoBD6vqiESAWT7gpHLAvgtjzsQOfW7kaIocAs";
+    [Header("시트의 주소")][SerializeField] public string associatedSheet = "";
     [Header("스프레드 시트의 시트 이름")][SerializeField] public string associatedWorksheet = "";
     [Header("읽기 시작할 행 번호")][SerializeField] public int START_ROW_LENGTH = 2;
     [Header("읽을 마지막 행 번호")][SerializeField] public int END_ROW_LENGTH = -1;
