2d5568e2bd5742721a100028fe0a0acfa28276c7
EunHaSong <shaeun03290@sookmyung.ac.kr>
2024-07-29 04:15:35 +0900

    feat. 데이터 저장 기능 추가 완료

--- Assets/Scripts/Haeun/DataLoad/DataManager.cs
+++ Assets/Scripts/Haeun/DataLoad/DataManager.cs
@@ -2,44 +2,75 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
+using System;
 using Unity.Profiling;
 using UnityEngine.UIElements;
+using System.Data.Common;
+
+[System.Serializable]
+public class Item
+{
+    public Item(string _Id, string _Name, string _Description, string _Type, bool _isUsing, string _Price, bool _isSelling, string _value, string _quantity)
+    {
+        Id = _Id; Name = _Name; Description = _Description; Type = _Type; isUsing = _isUsing; Price = _Price; 
+        isSelling = _isSelling; value = _value; quantity = _quantity;
+    }
+
+    public string Id, Name, Description, Type, Price;
+    public bool isUsing, isSelling;
+    public string value, quantity;
+}
+
+[System.Serializable]
+public class Character
+{
+    public Character(string _Id, string _Name, string _Description, string _HP, string _STR, string _DEX, string _INT, string _CON, string _DEF, string _ATK, bool _isUsing, string _Type)
+    {
+        Id = _Id; Name = _Name; Description = _Description; 
+        HP = _HP; STR = _STR; DEX = _DEX; 
+        INT = _INT; CON = _CON; DEF = _DEF; ATK = _ATK;
+        isUsing = _isUsing; Type = _Type;
+    }
+
+    // 캐릭터 관련 변수들
+    public string Id, Name, Description, HP, STR, DEX, INT, CON, DEF, ATK;
+    public string Type;
+    public bool isUsing;
+}
+
+[System.Serializable]
+public class PlayerData
+{
+    // 일단 이런 데이터들을 모아두기
+    public string Player_name = "용사님";             // 플레이어 이름 데이터 변수
+    public int Player_day = 1;              // 날짜 데이터 변수
+    public int Player_team = 0;             // 단합력 데이터 변수
+    public int Player_hp = 100;               // 체력 데이터 변수
+    public int Player_tired = 0;            // 피로도 데이터 변수
+    public int Player_money = 0;            // 피로도 데이터 변수
+    public int Player_hint = 0;             // 힌트 데이터 변수
+    public int Player_howtoday = 0;            // 기본 활동 일차 데이터 변수
+    public int Player_howtrain = 0;            // 기본 활동 훈련 변수 데이터 변수
+
+    // 캐릭터 관련
+    public List<Character> characters = new List<Character>();
+
+    // 아이템 관련
+    public List<Item> Items = new List<Item>();
+}
 
 public class DataManager : MonoBehaviour
 {
-    // 저장하는 방법
-    // 1. 저장할 데이터가 존재
-    // 2. 데이터를 제이슨으로 변환
-    // 3. 제이슨을 외부에 저장
-
-    // 불러오는 방법
-    // 1. 외부에 저장된 제이슨을 가져옴
-    // 2. 제이슨을 데이터 형태로 변환
-    // 3. 불러온 데이터를 사용
-
-
-    public class PlayerData
-    {
-        // 일단 이런 데이터들을 모아두기
-        public string Player_name;             // 플레이어 이름 데이터 변수
-        public string Player_day;              // 날짜 데이터 변수
-        public string Player_team;             // 단합력 데이터 변수
-        public string Player_hp;               // 체력 데이터 변수
-        public string Player_tired;            // 재화 데이터 변수
-        public string Player_hint;            // 재화 데이터 변수
-        public string Player_howtoday;            // 재화 데이터 변수
-        public string Player_howtrain;            // 재화 데이터 변수
-        
-
-        // 캐릭터 관련
-
-        // 아이템 관련
-
-    }
-
     public PlayerData nowPlayer = new PlayerData();
     public string path;
     public int nowSlot;
+    
+    // csv 정보 읽어오기 변수
+    [SerializeField] private List<Item> CSVitem = new List<Item>();
+    [SerializeField] private List<Character> CSVCharacter = new List<Character>();
+
+    // 데이터 변경 이벤트
+    public event Action OnDataChanged;
 
     // 싱글톤 구성 & 게임 오브젝트가 터지지 않도록 하게 함.
     public static DataManager instance;
@@ -53,22 +84,16 @@ public class DataManager : MonoBehaviour
         }
         DontDestroyOnLoad(this.gameObject); // 게임 오브젝트가 터지지 않도록
         #endregion
-        path = Application.persistentDataPath + "/saveData"; // 직접 경로를 설정하기 어려운 경우는, 유니티에서 기본적으로 제공하는 path 사용
-    }
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+        path = Application.persistentDataPath + "/saveGameData"; // 직접 경로를 설정하기 어려운 경우는, 유니티에서 기본적으로 제공하는 path 사용
     }
 
     public void SaveData()
-    {
+    {   
         // 데이터를 현재 폴더 내에 생성. 
         string data = JsonUtility.ToJson(nowPlayer);
-        print(path);
+        print(data);
         File.WriteAllText(path + nowSlot.ToString(), data);
+        OnDataChanged?.Invoke(); // 데이터 저장 시 이벤트 호출
     }
 
     public void LoadData() 
@@ -76,13 +101,95 @@ public class DataManager : MonoBehaviour
         // 데이터를 내가 원하는 형태로 가지고 올 수 있음. / 현재는 nowPlayer에 저장되어 있음.
         string data = File.ReadAllText(path + nowSlot.ToString());
         nowPlayer = JsonUtility.FromJson<PlayerData>(data); // 불러온 데이터가 PlayerData 형태로 저장되어 있음.
+        OnDataChanged?.Invoke(); // 데이터 로드 시 이벤트 호출
     }
 
     public void ClearData() {
-
         nowSlot = -1;
         // 이렇게 되면 현재 플레이어에 담긴 정보값이 모두 사라지게 된다.
         nowPlayer = new PlayerData();
+        OnDataChanged?.Invoke(); // 데이터 초기화 시 이벤트 호출
+    }
+
+    public void LoadItemsFromCSV(string fileName)
+    {
+        var data = CSVReader.Read(fileName);
+
+        if (data != null)
+        {
+            foreach (var entry in data)
+            {
+                string id = entry["id"].ToString();
+                string name = entry["name"].ToString();
+                string description = entry["description"].ToString();
+                string type = entry["type"].ToString();
+                bool isUsing, isSelling;
+                string price = entry["price"].ToString();
+
+                if (!bool.TryParse(entry["isUsing"].ToString(), out isUsing))
+                {
+                    isUsing = false; // 파싱 실패 시 기본값 설정
+                }
+                if (!bool.TryParse(entry["isSelling"].ToString(), out isSelling))
+                {
+                    isSelling = false; // 파싱 실패 시 기본값 설정
+                };
+                string value = entry["value"].ToString();
+                string quantity = entry["quantity"].ToString();
+
+                var newItem = new Item(id, name, description, type, isUsing, price, isSelling, value, quantity);
+                CSVitem.Add(newItem);
+                nowPlayer.Items.Add(newItem); // nowPlayer의 Items 리스트에 추가
+            }
+        }
+        else
+        {
+            Debug.LogError("CSV 데이터를 불러오지 못했습니다.");
+        }
+    }
+
+    public void LoadCharactersFromCSV(string fileName, string playerName)
+    {
+        var data = CSVReader.Read(fileName);
+
+        if (data != null)
+        {
+            foreach (var entry in data)
+            {
+                string name;
+                if (playerName != null && entry["name"].ToString() == "{player_name}") {
+                    name = playerName;
+                }
+                else {
+                    name = entry["name"].ToString();
+                }
+
+                // Name, Description, HP, STR, DEX, INT, CON, DEF, ATK, isUsing, Type;
+                string id = entry["id"].ToString();
+                string description = entry["description"].ToString();
+                string HP = entry["HP"].ToString();
+                string STR = entry["STR"].ToString();
+                string DEX = entry["DEX"].ToString();
+                string INT = entry["INT"].ToString();
+                string CON = entry["CON"].ToString();
+                string DEF = entry["DEF"].ToString();
+                string ATK = entry["ATK"].ToString();
+                string type = entry["type"].ToString();
+                bool isUsing;
+                
+                if (!bool.TryParse(entry["isUsing"].ToString(), out isUsing))
+                {
+                    isUsing = false; // 파싱 실패 시 기본값 설정
+                }
+
+                var newCharacter = new Character(id, name, description, HP, STR, DEX, INT, CON, DEF, ATK, isUsing, type);
+                CSVCharacter.Add(newCharacter);
+                nowPlayer.characters.Add(newCharacter); // nowPlayer의 characters 리스트에 추가
+            }
+        }
+        else
+        {
+            Debug.LogError("CSV 데이터를 불러오지 못했습니다.");
+        }
     }
 }
-
