f8c11a984d9a5603badb6170c477cf537983c504
jsssun <gmhie1208@gmail.com>
2024-07-30 19:15:41 +0900

    feat : deadendingScene 제작

---
+++ Assets/Scripts/Sunwoo/DeadEndingTalkManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro; // TextMeshPro 네임스페이스 추가
+
+public class DeadEndingTalkManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
