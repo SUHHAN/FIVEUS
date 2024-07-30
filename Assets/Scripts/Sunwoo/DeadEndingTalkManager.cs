6528349f90f58924d69dbe4cecb7b70c1c231939
jsssun <gmhie1208@gmail.com>
2024-07-30 23:44:53 +0900

    feat : DeadEndingTalkManager

--- Assets/Scripts/Sunwoo/DeadEndingTalkManager.cs
+++ Assets/Scripts/Sunwoo/DeadEndingTalkManager.cs
@@ -5,6 +5,11 @@ using TMPro; // TextMeshPro 네임스페이스 추가
 
 public class DeadEndingTalkManager : MonoBehaviour
 {
+
+    public GameObject opening;
+    public GameObject narration;
+    public TextMeshProUGUI narrationText; // TextMeshPro UI 텍스트 요소
+
     // Start is called before the first frame update
     void Start()
     {
