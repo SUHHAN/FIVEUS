bde4b94b109eaee9ebefdba57fa38acec9b6a586
jangeunbin <114203351+Eunbin618@users.noreply.github.com>
2024-08-03 23:26:58 +0900

    카메라 추가

--- Assets/Scripts/Yeji/PlayerAction_yj.cs
+++ Assets/Scripts/Yeji/PlayerAction_yj.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerAction_yj : MonoBehaviour
 {
     public float Speed_yj;
     public bool isAction_yj;
+
     Rigidbody2D rigid_yj;
     Animator play_anim_yj;
     Vector3 dirVec_yj;
@@ -14,46 +16,69 @@ public class PlayerAction_yj : MonoBehaviour
     float v_yj;
     bool isHorizonMove_yj;
 
-    public FixedJoystick joystick; // swÏ∂îÍ∞Ä. Ï°∞Ïù¥Ïä§Ìã±
+    private FixedJoystick joystick; // sw√ﬂ∞°. ¡∂¿ÃΩ∫∆Ω
 
+    public GameManager_bin manager_bin;
+    GameObject ScanObject_bin;
+    private bool collisionInfdoor = false;
+    private static PlayerAction_yj instance;
+    private bool isInputEnabled = true; // ¿‘∑¬ »∞º∫»≠ ªÛ≈¬∏¶ ¡¶æÓ«œ¥¬ ∫Øºˆ
     void Awake()
     {
-        // Ï¥àÍ∏∞Ìôî
+        // √ ±‚»≠
         rigid_yj = GetComponent<Rigidbody2D>();
         play_anim_yj = GetComponent<Animator>();
+
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
     }
 
     void Start()
     {
+        SceneManager.sceneLoaded += OnSceneLoaded;
 
+        FindJoystick();
     }
-
+    void OnDestroy()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
     void Update()
     {
-        // sw Ï∂îÍ∞Ä. Ï°∞Ïù¥Ïä§Ìã± ÏûÖÎ†• Î∞õÍ∏∞
-        h_yj = isAction_yj ? 0 : joystick.Horizontal;
-        v_yj = isAction_yj ? 0 : joystick.Vertical;
-
-        // sw Ï∂îÍ∞Ä. ÏûÖÎ†•Í∞íÏù¥ ÌäπÏ†ï Í∏∞Ï§Ä Ïù¥ÏÉÅÏù∏ÏßÄ ÌôïÏù∏
+        if (!isInputEnabled) return;
+        // sw √ﬂ∞°. ¡∂¿ÃΩ∫∆Ω ¿‘∑¬ πﬁ±‚
+        if (joystick != null)
+        {
+            h_yj = isAction_yj ? 0 : joystick.Horizontal;
+            v_yj = isAction_yj ? 0 : joystick.Vertical;
+        }
+        // sw √ﬂ∞°. ¿‘∑¬∞™¿Ã ∆Ø¡§ ±‚¡ÿ ¿ÃªÛ¿Œ¡ˆ »Æ¿Œ
         bool hDown_yj = isAction_yj ? false : Mathf.Abs(joystick.Horizontal) > 0.1f;
         bool vDown_yj = isAction_yj ? false : Mathf.Abs(joystick.Vertical) > 0.1f;
         bool hUp_yj = isAction_yj ? false : Mathf.Abs(joystick.Horizontal) <= 0.1f;
         bool vUp_yj = isAction_yj ? false : Mathf.Abs(joystick.Vertical) <= 0.1f;
 
-        if (hDown_yj)
-            isHorizonMove_yj = true;
-        else if (vDown_yj)
-            isHorizonMove_yj = false;
+        if (hDown_yj || vDown_yj)
+            isHorizonMove_yj = Mathf.Abs(h_yj) > Mathf.Abs(v_yj);
         else if (hUp_yj || vUp_yj)
+        {
             isHorizonMove_yj = h_yj != 0;
+        }
 
-        // Ïï†ÎãàÎ©îÏù¥ÏÖò
-        if (play_anim_yj.GetInteger("hAxisRaw_yj") != h_yj)
+        // æ÷¥œ∏ﬁ¿Ãº«
+        if (play_anim_yj.GetInteger("hAxisRaw_yj") != (int)h_yj)
         {
             play_anim_yj.SetBool("isChange_yj", true);
             play_anim_yj.SetInteger("hAxisRaw_yj", (int)h_yj);
         }
-        else if (play_anim_yj.GetInteger("vAxisRaw_yj") != v_yj)
+        else if (play_anim_yj.GetInteger("vAxisRaw_yj") != (int)v_yj)
         {
             play_anim_yj.SetBool("isChange_yj", true);
             play_anim_yj.SetInteger("vAxisRaw_yj", (int)v_yj);
@@ -61,20 +86,292 @@ public class PlayerAction_yj : MonoBehaviour
         else
             play_anim_yj.SetBool("isChange_yj", false);
 
-        dirVec_yj = new Vector3(h_yj, v_yj).normalized; // swÏ∂îÍ∞Ä. ÎåÄÍ∞ÅÏÑ† Ïù¥ÎèôÎèÑ Í∞ÄÎä•
+        dirVec_yj = new Vector3(h_yj, v_yj).normalized; // sw√ﬂ∞°. ¥Î∞¢º± ¿Ãµøµµ ∞°¥…
+
+        if (Input.GetButtonDown("Jump") && ScanObject_bin != null)
+            manager_bin.Action(ScanObject_bin);
+
+        if (collisionInfdoor && Input.GetButtonDown("Jump"))
+        {
+            StartCoroutine(LoadSceneCoroutine("inf_guild", 7));
+            collisionInfdoor = false;
+        }
     }
 
-    // Î¨ºÎ¶¨ Ïó∞ÏÇ∞ ÏóÖÎç∞Ïù¥Ìä∏
+    // π∞∏Æ ø¨ªÍ æ˜µ•¿Ã∆Æa
     void FixedUpdate()
     {
-        // Ïù¥Îèô Ï≤òÎ¶¨
-        if (dirVec_yj != Vector3.zero)
+        if (!isInputEnabled)
         {
-            rigid_yj.velocity = dirVec_yj * Speed_yj; // Î∞©Ìñ• Î≤°ÌÑ∞ÏôÄ ÏÜçÎèÑÎ•º Í≥±ÌïòÏó¨ ÏÜçÎèÑ ÏÑ§Ï†ï
+            rigid_yj.velocity = Vector2.zero; // æ¿ ∑ŒµÂ ¡ﬂ «√∑π¿ÃæÓ º”µµ 0¿∏∑Œ º≥¡§
+            return;
         }
+
+        rigid_yj.velocity = dirVec_yj * Speed_yj;
+
+        //ray
+        Debug.DrawRay(rigid_yj.position, dirVec_yj * 0.7f, new Color(0, 1, 0));
+        RaycastHit2D rayHit_bin = Physics2D.Raycast(rigid_yj.position, dirVec_yj, 0.7f, LayerMask.GetMask("Object"));
+
+        if (rayHit_bin.collider != null)
+            ScanObject_bin = rayHit_bin.collider.gameObject;
         else
+            ScanObject_bin = null;
+    }
+    public int LoadedScene;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "main_indoor")
         {
-            rigid_yj.velocity = Vector2.zero; // Ï†ïÏßÄ
+            StartCoroutine(LoadSceneCoroutine("main_house", 1));
         }
+        if (collision.gameObject.name == "main_outdoor")
+        {
+            LoadedScene = 1;
+            StartCoroutine(LoadSceneCoroutine("main_map", 0));
+        }
+        if (collision.gameObject.name == "store_indoor")
+        {
+            StartCoroutine(LoadSceneCoroutine("store", 2));
+        }
+        if (collision.gameObject.name == "store_outdoor")
+        {
+            LoadedScene = 2;
+            StartCoroutine(LoadSceneCoroutine("main_map", 0));
+        }
+        if (collision.gameObject.name == "bar_indoor")
+        {
+            StartCoroutine(LoadSceneCoroutine("bar", 3));
+        }
+        if (collision.gameObject.name == "bar_outdoor")
+        {
+            LoadedScene = 3;
+            StartCoroutine(LoadSceneCoroutine("main_map", 0));
+        }
+        if (collision.gameObject.name == "hotel_indoor")
+        {
+            StartCoroutine(LoadSceneCoroutine("hotel", 4));
+        }
+        if (collision.gameObject.name == "hotel_outdoor")
+        {
+            LoadedScene = 4;
+            StartCoroutine(LoadSceneCoroutine("main_map", 0));
+        }
+
+        if (collision.gameObject.name == "hotel_hall_indoor")
+        {
+            StartCoroutine(LoadSceneCoroutine("hotel_hall", 11));
+        }
+        if (collision.gameObject.name == "hotel_hall_outdoor")
+        {
+            LoadedScene = 11;
+            StartCoroutine(LoadSceneCoroutine("hotel", 4));
+        }
+        if (collision.gameObject.name == "hotel_room1_indoor")
+        {
+            StartCoroutine(LoadSceneCoroutine("hotel_room1", 12));
+        }
+        if (collision.gameObject.name == "hotel_room1_outdoor")
+        {
+            LoadedScene = 12;
+            StartCoroutine(LoadSceneCoroutine("hotel_hall", 11));
+        }
+        if (collision.gameObject.name == "hotel_room2_indoor")
+        {
+            StartCoroutine(LoadSceneCoroutine("hotel_room2", 13));
+        }
+        if (collision.gameObject.name == "hotel_room2_outdoor")
+        {
+            LoadedScene = 13;
+            StartCoroutine(LoadSceneCoroutine("hotel_hall", 11));
+        }
+        if (collision.gameObject.name == "sub1house_indoor")
+        {
+            StartCoroutine(LoadSceneCoroutine("sub1_house", 5));
+        }
+        if (collision.gameObject.name == "sub1house_outdoor")
+        {
+            LoadedScene = 5;
+            StartCoroutine(LoadSceneCoroutine("main_map", 0));
+        }
+        if (collision.gameObject.name == "magichouse_indoor")
+        {
+            StartCoroutine(LoadSceneCoroutine("magic_house", 6));
+        }
+        if (collision.gameObject.name == "magichouse_outdoor")
+        {
+            LoadedScene = 6;
+            StartCoroutine(LoadSceneCoroutine("main_map", 0));
+        }
+        if ((collision.gameObject.name == "infguild_indoor"))
+        {
+            collisionInfdoor = true;
+
+        }
+        if (collision.gameObject.name == "infguild_outdoor")
+        {
+            LoadedScene = 7;
+            StartCoroutine(LoadSceneCoroutine("main_map", 0));
+        }
+
+        if (collision.gameObject.name == "inf_indoor")
+        {
+            StartCoroutine(LoadSceneCoroutine("inf_guild_in", 8));
+        }
+        if (collision.gameObject.name == "inf_outdoor")
+        {
+            LoadedScene = 8;
+            StartCoroutine(LoadSceneCoroutine("inf_guild", 7));
+        }
+        if (collision.gameObject.name == "camping_indoor")
+        {
+            StartCoroutine(LoadSceneCoroutine("camping", 9));
+        }
+        if (collision.gameObject.name == "camping_outdoor")
+        {
+            LoadedScene = 9;
+            StartCoroutine(LoadSceneCoroutine("main_map", 0));
+        }
+        if (collision.gameObject.name == "training_indoor")
+        {
+            StartCoroutine(LoadSceneCoroutine("training", 10));
+        }
+        if (collision.gameObject.name == "training_outdoor")
+        {
+            LoadedScene = 10;
+            StartCoroutine(LoadSceneCoroutine("main_map", 0));
+        }
+
+    }
+
+    private IEnumerator LoadSceneCoroutine(string sceneName, int loadedScene)
+    {
+        isInputEnabled = false; // ¿‘∑¬ ∫Ò»∞º∫»≠
+        rigid_yj.velocity = Vector2.zero; // «√∑π¿ÃæÓ º”µµ 0¿∏∑Œ º≥¡§
+        yield return SceneManager.LoadSceneAsync(sceneName);
+        LoadedScene = loadedScene;
+        isInputEnabled = true; // ¿‘∑¬ »∞º∫»≠
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        Speed_yj = 0;
+        Debug.Log("Loaded scene: " + scene.name);
+
+        if (scene.name == "main_house")
+        {
+            transform.position = new Vector3(2, -2, 0);
+        }
+        if (scene.name == "main_map" && LoadedScene == 1)
+        {
+            transform.position = new Vector3(-20, 8, 0);
+        }
+        if (scene.name == "store")
+        {
+            transform.position = new Vector3(0, -3, 0);
+        }
+        if (scene.name == "main_map" && LoadedScene == 2)
+        {
+            transform.position = new Vector3(-23, -5, 0);
+        }
+        if (scene.name == "bar")
+        {
+            transform.position = new Vector3(0, -3, 0);
+        }
+        if (scene.name == "main_map" && LoadedScene == 3)
+        {
+            transform.position = new Vector3(-9, -14, 0);
+        }
+        if (scene.name == "hotel")
+        {
+            transform.position = new Vector3(0, -3, 0);
+        }
+        if (scene.name == "main_map" && LoadedScene == 4)
+        {
+            transform.position = new Vector3(17, 9, 0);
+        }
+        if (scene.name == "hotel_hall")
+        {
+            transform.position = new Vector3(1, -1, 0);
+        }
+        if (scene.name == "hotel" && LoadedScene == 11)
+        {
+            transform.position = new Vector3(1, 2, 0);
+        }
+        if (scene.name == "hotel_room1")
+        {
+            transform.position = new Vector3(1, 1, 0);
+        }
+        if (scene.name == "hotel_hall" && LoadedScene == 12)
+        {
+            transform.position = new Vector3(2, 1, 0);
+        }
+        if (scene.name == "hotel_room2")
+        {
+            transform.position = new Vector3(-1, 1, 0);
+        }
+        if (scene.name == "hotel_hall" && LoadedScene == 13)
+        {
+            transform.position = new Vector3(-4, 1, 0);
+        }
+        if (scene.name == "sub1_house")
+        {
+            transform.position = new Vector3(2, -2, 0);
+        }
+        if (scene.name == "main_map" && LoadedScene == 5)
+        {
+            transform.position = new Vector3(-9, 6, 0);
+        }
+        if (scene.name == "magic_house")
+        {
+            transform.position = new Vector3(2, -3, 0);
+        }
+        if (scene.name == "main_map" && LoadedScene == 6)
+        {
+            transform.position = new Vector3(-21, -14, 0);
+        }
+        if (scene.name == "inf_guild")
+        {
+            transform.position = new Vector3(2, -2, 0);
+        }
+        if (scene.name == "main_map" && LoadedScene == 7)
+        {
+            transform.position = new Vector3(19, -14, 0);
+        }
+        if (scene.name == "inf_guild_in")
+        {
+            transform.position = new Vector3(1, -2, 0);
+        }
+        if (scene.name == "inf_guild" && LoadedScene == 8)
+        {
+            transform.position = new Vector3(3, 4, 0);
+        }
+        if (scene.name == "camping")
+        {
+            transform.position = new Vector3(3, -1, 0);
+        }
+        if (scene.name == "main_map" && LoadedScene == 9)
+        {
+            transform.position = new Vector3(-9, -18, 0);
+        }
+        if (scene.name == "training")
+        {
+            transform.position = new Vector3(8, 1, 0);
+        }
+        if (scene.name == "main_map" && LoadedScene == 10)
+        {
+            transform.position = new Vector3(-26, 4, 0);
+        }
+
+        FindJoystick();
+
+        Speed_yj = 3; // ¿Ãµø º”µµ ∫πø¯
+    }
+
+    void FindJoystick()
+    {
+        joystick = FindObjectOfType<FixedJoystick>();
+        if(joystick==null)
+            Debug.LogError("FixedJoystick¿Ã æ¿ø° æ¯Ω¿¥œ¥Ÿ.");
     }
 }
